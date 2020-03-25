41c436461faf6e25eaa560022ee5a74262e3b539
LuizFurtado <luizfpfurtado@hotmail.com>
2020-03-24 22:48:09 -0300

    Included localization to some locations only

--- src/_data/news.js
+++ src/_data/news.js
@@ -1,13 +1,27 @@
 require('dotenv').config();
 
 const axios = require('axios');
+const countries = require('./countries.json');
 
-module.exports = async function getNews() {
+async function getNews(country) {
     try {
-        const response = await axios.get(`http://newsapi.org/v2/top-headlines?country=us&apiKey=${process.env.API_KEY}&pageSize=5`);
-        return response.data;
+        const response = await axios.get(`http://newsapi.org/v2/top-headlines?country=${country}&apiKey=${process.env.API_KEY}&pageSize=5`);
+        return {
+            "country": country,
+            "articles": response.data.articles
+        };
     }
     catch(err) {
         console.log(err);
     }
+}
+
+
+module.exports = async function () {
+    let newPromises = countries.map(getNews);
+    return Promise.all(newPromises)
+    .then( newsObjects => {
+        console.log('newsObjects', newsObjects);
+        return [].concat.apply([], newsObjects);
+    });
 }
